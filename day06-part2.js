7fe7d5ac4ac1c806546981a8d79c77d7fc9a57d7
dolliars <dolly.lanuza@gmail.com>
2020-12-07 01:25:49 -0500

    Not a bad attempt, but no dice

--- day06-part2.js
+++ day06-part2.js
@@ -13,8 +13,21 @@ function unanimousYes(data) {
   let yes = 0;
 
   data.forEach(function (element) {
-    console.log(element);
-    console.log(element.length);
+    let splicedLettersArray = [];
+    for(let i = 0; i < element.length; i++) {
+      let stringObj = element[i];
+      let string = stringObj[0];
+      string = string.split("");
+      splicedLettersArray.push(string)
+    }
+
+    console.log('new check');
+
+    for(let i = 0; i < splicedLettersArray.length - 1 ; i++) {
+      const filteredYes = splicedLettersArray[i].filter(value => splicedLettersArray[i+1].includes(value));
+      console.log(filteredYes);
+    }
+
   });
 }
 
@@ -23,4 +36,3 @@ unanimousYes(data);
 //const set1 = new Set([1, 2, 3, 4, 5]);
 //console.log(set1.has(1)); // expected output: true
 //console.log(set1.has(6));// expected output: false
-
